063d39ab76e1cf8b6e0372d0becd3d0d0ae15ee0
Lucas <mazalucas@hotmail.com>
2023-08-29 10:45:53 -0300

    feat(*): authjs v5, switch to private repo dep

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: "export",
-  swcMinify: false,
-  // TODO: see if removing this allows to use the package normally
-  webpack: (config) => {
-    config.resolve.preferRelative = true;
-    return config;
+  experimental: {
+    serverActions: true,
   },
 };
 
