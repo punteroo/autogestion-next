a3ef9ab831d1bfdfdca255012e6a743738387a45
Lucas <mazalucas@hotmail.com>
2023-11-22 00:16:33 -0300

    feat(*): disabled zoom on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,14 +22,39 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={`${inter.className} dark bg-background min-h-screen`}>
         <NextAuth session={session}>
           <NextUI>
-            <DashboardContextProvider>
-              {children}
-            </DashboardContextProvider>
+            <DashboardContextProvider>{children}</DashboardContextProvider>
           </NextUI>
         </NextAuth>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `document.addEventListener('gesturestart', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 0.99;
+        });
+        
+        document.addEventListener('gesturechange', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 0.99;
+        });
+        
+        document.addEventListener('gestureend', function(e) {
+            e.preventDefault();
+            // special hack to prevent zoom-to-tabs gesture in safari
+            document.body.style.zoom = 0.99;
+        });`,
+          }}
+        ></script>
       </body>
     </html>
   );
