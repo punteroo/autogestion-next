28e829730a981b62d7f5333e0f1f9dd33c1f7a8b
Lucas <mazalucas@hotmail.com>
2023-11-25 16:42:36 -0300

    fix(*): changed main text between devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ export default async function Home() {
     <MainDashboard>
       <div className="flex flex-col items-center justify-center mx-8 text-center">
         <h1 className="my-4 text-xl font-bold">Bienvenido a tu Autogestión</h1>
-        <h2 className="my-4 text-sm font-light text-foreground-300">
+        <h2 className="my-4 text-sm md:hidden font-light text-foreground-300">
           Comienza seleccionando una sección desde la barra en la izquierda.
+        </h2>
+        <h2 className="my-4 text-sm hidden md:block font-light text-foreground-300">
+          Comienza seleccionando una sección arriba.
         </h2>
       </div>
     </MainDashboard>
