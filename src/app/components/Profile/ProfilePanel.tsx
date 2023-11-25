b3191a9e111b84253ac9e747a21c1c456e896df8
Lucas <mazalucas@hotmail.com>
2023-11-25 15:21:46 -0300

    feat(profile): responsive profile page

--- src/app/components/Profile/ProfilePanel.tsx
+++ src/app/components/Profile/ProfilePanel.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Card, CardHeader, Divider, CardBody, Button } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  Divider,
+  CardBody,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { UserSession } from "../../api/auth/[...nextauth]/route";
 import { LockIcon } from "../Icons/LockIcon";
@@ -10,47 +17,62 @@ export default function ProfilePanel() {
   const user = session?.user as UserSession;
 
   return (
-    <div className="flex flex-col items-center justify-center mx-4 gap-4">
-      <h1 className="my-4 text-xl font-bold">Mi Cuenta</h1>
-      <Card className="w-full">
-        {user ? (
-          <>
-            <CardHeader className="flex gap-3">
-              <div className="flex flex-col">
-                <p className="text-md">
-                  {user?.lastName}, {user?.firstName}
-                </p>
-                <p className="text-small text-default-500">
-                  {user?.career?.name}
-                </p>
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <div className="flex flex-col">
-                <p className="text-md">Legajo #{user?.academicId}</p>
-                <p className="text-small text-default-500">DNI {user?.dni}</p>
-              </div>
-            </CardBody>
-          </>
-        ) : null}
-      </Card>
+    <>
+      <h1 className="hidden md:block text-3xl font-bold m-4 text-center">
+        Mi Cuenta
+      </h1>
+      <div className="flex max-md:flex-col items-center md:items-start justify-center md:justify-between md:flex mx-4 gap-4">
+        <h1 className="my-4 text-xl font-bold md:hidden">Mi Cuenta</h1>
+        <Card className="w-full">
+          {user ? (
+            <>
+              <CardHeader className="flex gap-3">
+                <div className="flex gap-4 items-center">
+                  <div className="hidden md:block">
+                    <Avatar
+                      isBordered
+                      radius="full"
+                      src="/avatar.jpg"
+                      size="md"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-md">
+                      {user?.lastName}, {user?.firstName}
+                    </p>
+                    <p className="text-small text-default-500">
+                      {user?.career?.name}
+                    </p>
+                  </div>
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+                <div className="flex flex-col">
+                  <p className="text-md">Legajo #{user?.academicId}</p>
+                  <p className="text-small text-default-500">DNI {user?.dni}</p>
+                </div>
+              </CardBody>
+            </>
+          ) : null}
+        </Card>
 
-      <Card className="w-full">
-        <CardHeader>
-          <h1 className="font-bold text-base m-auto">Acciones</h1>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <div className="grid auto-col-auto gap-x-2 gap-y-2 grid-flow-col">
-            <div className="mx-auto">
-              <Button color="danger" size="md" startContent={<LockIcon />}>
-                Cambiar Contraseña
-              </Button>
+        <Card className="w-full">
+          <CardHeader>
+            <h1 className="font-bold text-base m-auto">Acciones</h1>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <div className="grid auto-col-auto gap-x-2 gap-y-2 grid-flow-col">
+              <div className="mx-auto">
+                <Button color="danger" size="md" startContent={<LockIcon />}>
+                  Cambiar Contraseña
+                </Button>
+              </div>
             </div>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
+          </CardBody>
+        </Card>
+      </div>
+    </>
   );
 }
