f147b21e73e03fd4b57e1980aa5b20ed00a23f40
Lucas <mazalucas@hotmail.com>
2023-11-30 12:11:56 -0300

    Feat/firebase functions (#2)
    * feat(firebase): migrated academic status and exams to firebase
    
    * refactor(exam, academic): use public envs for hostname
    
    * fix(*): adjust turns missing error message handling
    
    * refactor(exams): removed skeleton for loading
    
    * feat(env): environments for new api hostnames
    
    * fix(typescript): exclude firebase project from tsconfig

---
+++ functions/src/types/academic.entry.d.ts
@@ -0,0 +1,28 @@
+import { AcademicStatusEntry } from "autogestion-frvm/dist/types";
+
+/**
+ * A better formatted academic entry for consumption within the app.
+ *
+ * @interface
+ */
+export interface AcademicEntry {
+  id: number;
+
+  name: string;
+
+  plan: number;
+
+  level: number;
+
+  status: AcademicStatusEntry["estadoMateria"] | "PROMOCIONADA";
+
+  grade: number;
+
+  abscenses: Array<string>;
+
+  passPending: Array<Omit<AcademicEntry, "passPending" | "regularizePending">>;
+
+  regularizePending: Array<
+    Omit<AcademicEntry, "passPending" | "regularizePending">
+  >;
+}
